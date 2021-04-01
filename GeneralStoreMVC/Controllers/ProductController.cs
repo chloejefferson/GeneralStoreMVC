b5ebe71640f8ba13d6486299f511f5fe605d888f
chloejefferson <73564563+chloejefferson@users.noreply.github.com>
2021-04-01 18:41:04 -0400

    Detail functionality

--- GeneralStoreMVC/Controllers/ProductController.cs
+++ GeneralStoreMVC/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using GeneralStoreMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -65,6 +66,52 @@ namespace GeneralStoreMVC.Controllers
             _db.Products.Remove(product);
             _db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        //GET: Edit
+        // Product/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        //POST: Edit
+        // Product/Edit/{id}
+        [HttpPost, ActionName("Edit"), ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(product).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        //GET: Details
+        // Product/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
         }
     }
 }
